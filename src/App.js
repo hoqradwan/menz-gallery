0a84e28c6eaf56cb65640a161a0226b1e32d5d21
hoqradwan <92720128+hoqradwan@users.noreply.github.com>
2022-08-25 06:14:30 +0600

    project completion previous stage

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useEffect, useReducer } from "react";
+import Cart from "./components/Cart";
+import Navbar from "./components/Navbar";
+import Products from "./components/Products";
+import { cartReducer } from "./reducers/cartReducer";
 
 function App() {
+  const [state, dispatch] = useReducer(cartReducer, {
+    products: [],
+    cart: [],
+  });
+  const fetchProducts = async () => {
+    // const { data } = await axios("https://dummyjson.com/products");
+    const { data } = await axios("https://fakestoreapi.com/products");
+    /* const { data } = await axios("'https://fakestoreapi.com/products/category/jewelery"); */
+
+    dispatch({
+      type: "ADD_PRODUCT",
+      payload: data,
+    });
+    console.log(data)
+  };
+  useEffect(() => {
+    fetchProducts();
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Navbar></Navbar>
+      <div style={{ display: "flex" }}>
+        <Products state={state} dispatch={dispatch}></Products>
+        <Cart state={state} dispatch={dispatch}></Cart>
+      </div>
     </div>
   );
 }
---
+++ src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <div style={{
+           backgroundColor: "#33343B",
+           height: "60px",
+           marginTop: "0" ,
+           color: "#D2D3D5",
+           textAlign: "center",
+           fontSize: "30px",
+           fontWeight: "semi-bold",
+           paddingTop: "8px"
+          
+
+        }}>
+           Menz Gallery
+        </div>
+    );
+};
+
+export default Navbar;
